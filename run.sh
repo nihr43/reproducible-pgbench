9e37faff697d82b5e6d6a654af6f1e27e75d4aca
Nathan Hensel <nathanihensel@gmail.com>
2020-09-14 05:29:52 +0000

    clean up terraform output for jenkins

--- run.sh
+++ run.sh
@@ -5,11 +5,11 @@ set -euxo pipefail
 hash terraform pgbench
 source vars.env
 
-terraform init
-terraform apply --auto-approve
+terraform init -no-color
+terraform apply -auto-approve -no-color
 
 export PGPASSWORD=$TF_VAR_PGBENCH_DB_PASS
 time pgbench -h $TF_VAR_PGBENCH_DB_HOST -U $TF_VAR_PGBENCH_DB_USER -i -s 10000 pgbench
 pgbench -h $TF_VAR_PGBENCH_DB_HOST -U $TF_VAR_PGBENCH_DB_USER -c 64 -j 4 -T 3600 pgbench
 
-terraform destroy -auto-approve
+terraform destroy -auto-approve -no-color
