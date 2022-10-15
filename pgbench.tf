5ae3843d0fa3f8298502e86bba95cf31f28e5de8
nathan hensel <nathanihensel@gmail.com>
2022-10-15 12:26:17 -0500

    update provider import

--- pgbench.tf
+++ pgbench.tf
@@ -2,6 +2,15 @@ variable "PGBENCH_DB_HOST" {}
 variable "PGBENCH_DB_USER" {}
 variable "PGBENCH_DB_PASS" {}
 
+terraform {
+  required_providers {
+    postgresql = {
+      source = "cyrilgdn/postgresql"
+      version = "1.17.1"
+    }
+  }
+}
+
 provider "postgresql" {
   host            = var.PGBENCH_DB_HOST
   port            = 5432
